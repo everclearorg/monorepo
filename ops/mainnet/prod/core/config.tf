6dfbf3020a0f7c685669fe5bb3daa106db5df663
preethamr <preethamr@users.noreply.github.com>
2025-01-31 10:47:09 -0800

    fix: temporary fix for lambda config limits

--- ops/mainnet/prod/core/config.tf
+++ ops/mainnet/prod/core/config.tf
@@ -316,49 +316,41 @@ locals {
       "1" = {
         providers = [
           "https://eth-mainnet.blastapi.io/${var.blast_key}",
-          "https://eth-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "56" = {
         providers = [
           "https://bsc-mainnet.blastapi.io/${var.blast_key}",
-          "https://bnb-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "42161" = {
         providers = [
           "https://arbitrum-one.blastapi.io/${var.blast_key}",
-          "https://arb-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "10" = {
         providers = [
           "https://optimism-mainnet.blastapi.io/${var.blast_key}",
-          "https://opt-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "8453" = {
         providers = [
-          "https://base-mainnet.g.alchemy.com/v2/${var.alchemy_key}",
           "https://base-mainnet.blastapi.io/${var.blast_key}"
         ]
       }
       "48900" = {
         providers = [
           "https://lb.drpc.org/ogrpc?network=zircuit-mainnet&dkey=${var.drpc_key}",
-          "https://zircuit1-mainnet.p2pify.com"
         ]
       }
       "81457" = {
         providers = [
           "https://lb.drpc.org/ogrpc?network=blast&dkey=${var.drpc_key}",
-          "https://blastl2-mainnet.public.blastapi.io"
         ]
       }
       "59144" = {
         providers = [
           "https://linea-mainnet.blastapi.io/${var.blast_key}",
-          "https://linea-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       # "324" = {
@@ -370,12 +362,10 @@ locals {
       "137" = {
         providers = [
           "https://polygon-mainnet.blastapi.io/${var.blast_key}",
-          "https://polygon-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "534352" = {
         providers = [
-          "https://scroll-mainnet.g.alchemy.com/v2/${var.alchemy_key}",
           "https://scroll-mainnet.blastapi.io/${var.blast_key}"
         ]
       }
@@ -387,13 +377,11 @@ locals {
       "33139" = {
         providers = [
           "https://apechain-mainnet.blastapi.io/${var.blast_key}",
-          "https://apechain-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "43114" = {
         providers = [
           "https://ava-mainnet.blastapi.io/${var.blast_key}/ext/bc/C/rpc",
-          "https://avalanche-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
     }
@@ -484,54 +472,49 @@ locals {
       59144 = { maxAge = 60, size = 1 },
       137 = { maxAge = 60, size = 1 },
       43114 = { maxAge = 60, size = 1 },
+      534352 = { maxAge = 60, size = 1 },
+      167000 = { maxAge = 60, size = 1 },
+      33139 = { maxAge = 60, size = 1 }
     }
     chains = {
       "1" = {
         providers = [
           "https://eth-mainnet.blastapi.io/${var.blast_key}",
-          "https://eth-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "56" = {
         providers = [
           "https://bsc-mainnet.blastapi.io/${var.blast_key}",
-          "https://bnb-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "42161" = {
         providers = [
           "https://arbitrum-one.blastapi.io/${var.blast_key}",
-          "https://arb-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "10" = {
         providers = [
           "https://optimism-mainnet.blastapi.io/${var.blast_key}",
-          "https://opt-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "8453" = {
         providers = [
           "https://base-mainnet.g.alchemy.com/v2/${var.alchemy_key}",
-          "https://base-mainnet.blastapi.io/${var.blast_key}"
         ]
       }
       "48900" = {
         providers = [
           "https://lb.drpc.org/ogrpc?network=zircuit-mainnet&dkey=${var.drpc_key}",
-          "https://zircuit1-mainnet.p2pify.com"
         ]
       }
       "81457" = {
         providers = [
           "https://lb.drpc.org/ogrpc?network=blast&dkey=${var.drpc_key}",
-          "https://blastl2-mainnet.public.blastapi.io"
         ]
       }
       "59144" = {
         providers = [
           "https://linea-mainnet.blastapi.io/${var.blast_key}",
-          "https://linea-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       # "324" = {
@@ -543,12 +526,10 @@ locals {
       "137" = {
         providers = [
           "https://polygon-mainnet.blastapi.io/${var.blast_key}",
-          "https://polygon-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "534352" = {
         providers = [
-          "https://scroll-mainnet.g.alchemy.com/v2/${var.alchemy_key}",
           "https://scroll-mainnet.blastapi.io/${var.blast_key}"
         ]
       }
@@ -560,13 +541,11 @@ locals {
       "33139" = {
         providers = [
           "https://apechain-mainnet.blastapi.io/${var.blast_key}",
-          "https://apechain-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
       "43114" = {
         providers = [
           "https://ava-mainnet.blastapi.io/${var.blast_key}/ext/bc/C/rpc",
-          "https://avalanche-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
         ]
       }
     }
