46abc8485baec2dc616f33941454742182959137
preethamr <preethamr@users.noreply.github.com>
2025-02-17 11:12:59 -0800

    fix: update gelato address exception list

--- packages/utils/src/peripherals/gelato.ts
+++ packages/utils/src/peripherals/gelato.ts
@@ -6,8 +6,14 @@ export const GELATO_SERVER = 'https://api.gelato.digital';
 // - On zkSync: 0x0c1B63765Be752F07147ACb80a7817A8b74d9831
 // So, for testnets you can already update the whitelist to these new addresses.
 
-export const getGelatoRelayerAddress = (domain: string): string =>
-  domain === '2053862260' || // zksync testnet
-  domain === '2053862243' // zksync mainnet
-    ? '0x0c1B63765Be752F07147ACb80a7817A8b74d9831'
-    : '0xF9D64d54D32EE2BDceAAbFA60C4C438E224427d0'; // all other networks
+export const getGelatoRelayerAddress = (domain: string): string => {
+  switch (domain) {
+    case '280': // zkSync testnet
+    case '324': // zkSync mainnet
+      return '0x0c1B63765Be752F07147ACb80a7817A8b74d9831';
+    case '130': // Unichain
+      return '0xC6e576260853e8eDb7a683Ff1233747Ad9904f16';
+    default:
+      return '0xF9D64d54D32EE2BDceAAbFA60C4C438E224427d0'; // all other networks
+  }
+};
