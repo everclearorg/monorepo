bc545f6d347be0dc82708a173ccf9fd41df47e70
0xHarbs <jamesharbridge@outlook.com>
2025-02-17 16:05:08 +0100

    feat: zk deployment files

--- packages/contracts/zk/src/contracts/common/Gateway.sol
+++ packages/contracts/zk/src/contracts/common/Gateway.sol
@@ -75,7 +75,7 @@ abstract contract Gateway is GasTank, IGateway, IMessageRecipient, ISpecifiesInt
 
         if (_unusedFee > 0) {
             // NOTE: Updated to msg.sender i.e. the receiver of unused fee would be the Spoke (due to compiler issue)
-            (bool _success,) = msg.sender.call{value: _unusedFee}("");
+            (bool _success,) = tx.origin.call{value: _unusedFee}("");
             if (!_success) revert Gateway_SendMessage_UnsuccessfulRebate();
         }
     }
