cae3fae73c925d3f8b10e8cf0e740925df4f38a8
Harry Lee Chak Chiu <harrier1126@gmail.com>
2025-02-18 04:11:16 -0500

    chore: cargo fmt

--- packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/admin.rs
+++ packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/admin.rs
@@ -2,7 +2,8 @@ use anchor_lang::prelude::*;
 
 use crate::{
     events::{
-        GatewayUpdatedEvent, LighthouseUpdatedEvent, MailboxUpdatedEvent, MessageGasLimitUpdatedEvent, WatchtowerUpdatedEvent
+        GatewayUpdatedEvent, LighthouseUpdatedEvent, MailboxUpdatedEvent,
+        MessageGasLimitUpdatedEvent, WatchtowerUpdatedEvent,
     },
     state::SpokeState,
 };
