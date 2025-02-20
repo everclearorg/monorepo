f80d5d776a23229d27acbe991ad2854dd0ef8b5e
Layne Haber <layne.haber@gmail.com>
2025-02-20 14:24:14 -0700

    fix: refactor with helpers

--- packages/contracts/test/integration/InvoiceHelper.t.sol
+++ packages/contracts/test/integration/InvoiceHelper.t.sol
@@ -34,11 +34,17 @@ contract InvoiceHelper is TestExtended {
   // Set up fee variables
   uint256 DBPS_DENOMINATOR = 100_000;
   uint256 MAX_FEE = 12;
+
+  // Setup environment variables (from mainnet prod)
   EverclearHub _hub = EverclearHub(0xa05A3380889115bf313f1Db9d5f335157Be4D816);
   HubGateway _hubGateway = HubGateway(payable(0xEFfAB7cCEBF63FbEFB4884964b12259d4374FaAa));
 
   function _applyMaxDiscount(uint256 _toDiscount) private returns (uint256 _discounted) {
     _discounted = (_toDiscount * (DBPS_DENOMINATOR - MAX_FEE)) / DBPS_DENOMINATOR;
+  }
+
+  function _applyDiscount(uint256 _toDiscount, uint256 _fee) private returns (uint256 _discounted) {
+    _discounted = (_toDiscount * (DBPS_DENOMINATOR - _fee)) / DBPS_DENOMINATOR;
   }
 
   function _setupFork(uint256 _l2Block, uint256 _l1Block, bool _etchSettler) private {
@@ -58,6 +64,11 @@ contract InvoiceHelper is TestExtended {
     vm.etch(_current, address(_settler).code);
   }
 
+  /**
+   * @notice Calculates the amount in, given some target amount after fees
+   * @param _amountAfterFees The target amount post fees
+   * @param _tickerHash The ticker hash for the fee config
+   */
   function _applyFees(uint256 _amountAfterFees, bytes32 _tickerHash) private returns (uint256 _amount) {
     IHubStorage.Fee[] memory _fees = _hub.tokenFees(_tickerHash);
     uint24 _totalFeeDbps;
@@ -69,6 +80,12 @@ contract InvoiceHelper is TestExtended {
     require(_amount > _amountAfterFees, 'Fees reduced amount');
   }
 
+  /**
+   * Calculates all of the deposits that _could_ be applied to invoices that have
+   * not yet been tabulated
+   * @param _tickerHash The ticker for the deposits to sum
+   * @param _domain The domain of the unprocessed deposits
+   */
   function _getTotalUnprocessedDeposits(bytes32 _tickerHash, uint32 _domain) private returns (uint256 _deposits) {
     uint48 _lastClosedEpoch = _hub.lastClosedEpochsProcessed(_tickerHash);
     uint48 _current = _hub.getCurrentEpoch();
@@ -81,6 +98,16 @@ contract InvoiceHelper is TestExtended {
     }
   }
 
+  /**
+   * Returns a message to be receivedo on the hub with a single intent to be used to purchase
+   * some invoice.
+   * @param _origin Origin of the intent
+   * @param _amountB Amount of the intent
+   * @param _inputAsset Asset for the intent
+   * @param _tickerHash Ticker for the intent
+   * @return _intentBatch The batch (n=1) of intents
+   * @return _intentIdB The identifier of the intent in the batch
+   */
   function _constructIntentBBatch(
     uint32 _origin,
     uint256 _amountB,
@@ -144,19 +171,58 @@ contract InvoiceHelper is TestExtended {
     // Get the custodied assets
     uint256 _custodied = _hub.custodiedAssets(_hub.assetHash(_tickerHash, _settlementDomain));
 
+    // console.log('required after fees/rewards', _invoiceAmount - _custodied);
+
     // Calculate the amount after fees
     _deposit = (DBPS_DENOMINATOR * (_invoiceAmount - _custodied)) / (DBPS_DENOMINATOR + MAX_FEE);
+
+    // console.log('c0', _custodied);
+    uint256 toBeDiscounted = _applyFees(_deposit, _tickerHash);
+    uint256 rewards = (toBeDiscounted * MAX_FEE) / DBPS_DENOMINATOR;
+    // console.log('[test] depositsAmount', toBeDiscounted);
+    // console.log('[test] invoiceAmount', _invoiceAmount);
+    // console.log('[test] liquidity', _custodied + toBeDiscounted);
+    // console.log('[test] rewards', rewards);
+    // console.log('[test] amount to settle', _invoiceAmount - rewards);
+    // console.log('amount to be discounted', toBeDiscounted);
+    require(_custodied + toBeDiscounted > _invoiceAmount - rewards);
   }
 
-  function test_intentPurchase() public {
-    uint256 _l1Block = 21876587;
-    _setupFork(784837, _l1Block, false);
+  function _receiveBatchIntentMessage(uint32 _messageOrigin, IEverclear.Intent[] memory _intentBatch) private {
+    // Format message to receive with proper amount
+    bytes memory _message = MessageLib.formatIntentMessageBatch(_intentBatch);
+    // Receive the message on the hub
+    vm.startPrank(address(_hubGateway.mailbox()));
+    _hubGateway.handle(_messageOrigin, _hubGateway.chainGateways(_messageOrigin), _message);
+    vm.stopPrank();
+  }
 
-    // Define intent constants
+  // ================================================
+  // ================ Test Cases ====================
+  // ================================================
+
+  /**
+   * @notice IntentA already exists at the front of the queue, there is one deposit with insufficient
+   * balance to settle the intent waiting to be processed.
+   */
+  function test_intentPurchaseSecondDeposit() public {}
+
+  /**
+   * @notice IntentA already exists at the front of the queue, and there are no deposits to be
+   * processed. Will calculate and verify the amountB to purchase a given invoice.
+   */
+  function test_intentPurchaseFirstInvoiceNoDeposits() public {
+    // Declare test constants
+    uint256 _l1Block = 21883550;
+    uint256 _l2Block = 790616;
     bytes32 _tickerHash = 0x8b1a1d9c2b109e527c9134b25b1a1833b16b6594f92daa9f6d9b7a6024bce9d0;
-    bytes32 _intentA = 0xdca32b8cd9553a81b4548540f141b23432b18ff1a28c0006870032097d313ba8;
-    uint256 _amountA = 501810314592980000000;
-    uint32 _intentADestination = 8453;
+    bytes32 _intentA = 0xcb0bd6c7aaca084e84c9f1153bd801e1378fff99b5fa8f273076fa5195ec5242;
+    uint256 _amountA = 8999820000000000000; // invoice amount
+    uint32 _intentADestination = 324;
+    address _intentBInputAsset = 0x493257fD37EDB34451f62EDf8D2a0C418852bA4C;
+
+    // Create fork
+    _setupFork(_l2Block, _l1Block, true);
 
     // Verify intentA exists
     require(_hub.contexts(_intentA).status == IEverclear.IntentStatus.INVOICED, 'intentA not invoiced');
@@ -165,31 +231,19 @@ contract InvoiceHelper is TestExtended {
     uint256 _amountB = _calculateDepositAmount(_intentADestination, _amountA, _tickerHash);
     // uint256 _amountB = (_amountA * DBPS_DENOMINATOR) / (DBPS_DENOMINATOR + MAX_FEE);
     console.log('amountB    :', _amountB, _applyFees(_amountB, _tickerHash));
+    console.log('intentB amt:', _applyFees(_amountB, _tickerHash));
 
-    // // Reduce by the custodied assets
-    // uint256 _custodied = _hub.custodiedAssets(_hub.assetHash(_tickerHash, _intentADestination));
-    // _amountB -= _custodied;
-    // console.log('custodied  :', _amountB, _custodied);
-
-    // // Get any deposits in the queue ahead of this one
-    // uint256 _unprocessed = _getTotalUnprocessedDeposits(_tickerHash, _intentADestination);
-    // _amountB -= _unprocessed;
-    // console.log('unprocessed:', _amountB, _unprocessed);
-
+    // Create the intent to settle the invoice
     (IEverclear.Intent[] memory _intentBBatch, bytes32 _intentIdB) = _constructIntentBBatch(
       _intentADestination,
       _amountB,
-      0x000000000000000000000000fde4C96c8593536E31F229EA8f37b2ADa2699bb2,
+      TypeCasts.toBytes32(_intentBInputAsset),
       _tickerHash
     );
     console.log('intentB:');
     console.logBytes32(_intentIdB);
 
-    // Format message to receive with proper amount
-    bytes memory _message = MessageLib.formatIntentMessageBatch(_intentBBatch);
-    // Receive the message on the hub
-    vm.prank(0x7f50C5776722630a0024fAE05fDe8b47571D7B39);
-    _hubGateway.handle(_intentADestination, TypeCasts.toBytes32(0x9ADA72CCbAfe94248aFaDE6B604D1bEAacc899A7), _message);
+    _receiveBatchIntentMessage(_intentADestination, _intentBBatch);
     // NOTE: if intentB is _not_ enqueued (`receiveMessage` is called) in the same epoch that intentA is
     // settled in, it will not get any rewards.
 
@@ -209,6 +263,7 @@ contract InvoiceHelper is TestExtended {
     // TODO: calculate rewards properly
     require(_hub.contexts(_intentIdB).pendingRewards > 0, 'intentB did not get rewards');
 
+    // Verify intentB was settled (not important)
     // // Advance to the next epoch
     // uint48 _initialEpoch = _hub.getCurrentEpoch();
     // uint48 _iterations;
