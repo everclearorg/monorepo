3f186fb99109ac9580fe18f48fb96fc5f9807f3d
Harry Lee Chak Chiu <harrier1126@gmail.com>
2025-02-24 10:10:29 -0500

    fix: compile error from the handle change

--- packages/contracts/solana-spoke/programs/everclear_spoke/src/hyperlane/primitive_type.rs
+++ packages/contracts/solana-spoke/programs/everclear_spoke/src/hyperlane/primitive_type.rs
@@ -1,10 +1,10 @@
-use anchor_lang::prelude::borsh::{self, BorshDeserialize, BorshSerialize};
+use anchor_lang::{prelude::borsh::{self, BorshDeserialize, BorshSerialize}, AnchorDeserialize, AnchorSerialize};
 use fixed_hash::construct_fixed_hash;
 use uint::construct_uint;
 
 construct_fixed_hash! {
     /// 256-bit hash type.
-    #[derive(BorshSerialize, BorshDeserialize)]
+    #[derive(AnchorSerialize, AnchorDeserialize)]
     pub struct H256(32);
 }
 
