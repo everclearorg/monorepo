5fe158a39bc44c36babb4843c23c776fe6fdb970
Oleg Tsybizov <otsybizov@gmail.com>
2025-02-24 21:00:49 -0600

    feat: added solana new_intent test

--- packages/contracts/solana-spoke/tests/everclear_spoke.ts
+++ packages/contracts/solana-spoke/tests/everclear_spoke.ts
@@ -1,21 +1,29 @@
-import * as anchor from "@coral-xyz/anchor";
-import { EverclearSpoke } from "../target/types/everclear_spoke";
-import { expect } from "@chimera-monorepo/utils";
+import * as anchor from '@coral-xyz/anchor';
+import { EverclearSpoke } from '../target/types/everclear_spoke';
+import { expect } from '@chimera-monorepo/utils';
+import * as token from '@solana/spl-token';
 
-describe("#everclear_spoke", () => {
+describe('#everclear_spoke', () => {
   anchor.setProvider(anchor.AnchorProvider.local());
+  const connection = anchor.getProvider().connection;
   const program = anchor.workspace.EverclearSpoke as anchor.Program<EverclearSpoke>;
   const [spokeStateAddress, _] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from('spoke-state')], program.programId);
 
   const lighthouseKeyPair = anchor.web3.Keypair.generate();
   const watchtowerKeyPair = anchor.web3.Keypair.generate();
-  const gatewayKeyPair = anchor.web3.Keypair.generate();
   const mailboxKeyPair = anchor.web3.Keypair.generate();
   const ownerKeyPair = anchor.web3.Keypair.generate();
-  const initialMessageGasLimit = new anchor.BN(10000);
+  const mint = anchor.web3.Keypair.generate();
+  const vault = anchor.web3.Keypair.generate();
+  const user = anchor.web3.Keypair.generate();
+  const payer = anchor.Wallet.local().payer;
 
-  describe("#initialize", () => {
-    it("should work", async () => {
+  const intentAmount = new anchor.BN(1000);
+  const initialMessageGasLimit = new anchor.BN(10000);
+  const TOKEN_DECIMALS = 18;
+
+  describe('#initialize', () => {
+    it('should work', async () => {
       // Arrange
       const params = {
         domain: 1,
@@ -24,7 +32,6 @@ describe("#everclear_spoke", () => {
         watchtower: watchtowerKeyPair.publicKey,
         callExecutor: anchor.web3.Keypair.generate().publicKey,
         messageReceiver: anchor.web3.Keypair.generate().publicKey,
-        gateway: gatewayKeyPair.publicKey,
         messageGasLimit: initialMessageGasLimit,
         owner: ownerKeyPair.publicKey,
         mailbox: mailboxKeyPair.publicKey,
@@ -43,7 +50,6 @@ describe("#everclear_spoke", () => {
       expect(spokeState.watchtower.toBase58()).to.be.equal(watchtowerKeyPair.publicKey.toBase58());
       expect(spokeState.callExecutor.toBase58()).to.be.equal(params.callExecutor.toBase58());
       expect(spokeState.messageReceiver.toBase58()).to.be.equal(params.messageReceiver.toBase58());
-      expect(spokeState.gateway.toBase58()).to.be.equal(gatewayKeyPair.publicKey.toBase58());
       expect(spokeState.messageGasLimit.toString()).to.be.equal(params.messageGasLimit.toString());
       expect(spokeState.owner.toBase58()).to.be.equal(ownerKeyPair.publicKey.toBase58());
       expect(spokeState.mailbox.toBase58()).to.be.equal(mailboxKeyPair.publicKey.toBase58());
@@ -52,8 +58,81 @@ describe("#everclear_spoke", () => {
     });
   });
 
-  describe("#pause", () => {
-    it("should work", async () => {
+  describe('#new_intent', () => {
+
+    it('should work', async () => {
+      // Arrange
+
+      // Create a mint account
+      const mintPubkey = await token.createMint(
+        connection,
+        payer, // fee payer
+        mint.publicKey, // mint authority
+        mint.publicKey, // freeze authority
+        TOKEN_DECIMALS, // decimals
+      );
+
+      // Create a user token account
+      const userTokenAccount = await token.createAssociatedTokenAccount(
+        connection,
+        payer, // fee payer
+        mintPubkey, // mint
+        user.publicKey, // owner,
+      );
+
+      // Mint some tokens to the user token account
+      await token.mintToChecked(
+        connection,
+        payer, // fee payer
+        mintPubkey, // mint
+        userTokenAccount, // receiver (should be a token account)
+        mint, // mint authority
+        50000, // amount
+        TOKEN_DECIMALS, // decimals
+      );
+
+      // Create a program vault account
+      const programVaultAccount = await token.createAssociatedTokenAccount(
+        connection,
+        payer, // fee payer
+        mintPubkey, // mint
+        vault.publicKey, // owner,
+      );
+
+      // Act
+      await program.methods.newIntent(
+        anchor.web3.Keypair.generate().publicKey, // receiver
+        anchor.web3.Keypair.generate().publicKey, // input_asset
+        anchor.web3.Keypair.generate().publicKey, // output_asset
+        intentAmount, // amount
+        123, // max_fee
+        new anchor.BN(0), // ttl
+        [1], // destinations
+        Buffer.from(''), // data
+        new anchor.BN(4321) // message_gas_limit
+      )
+      .accounts({
+        spokeState: spokeStateAddress,
+        payer: payer.publicKey,
+        authority: user.publicKey,
+        mint: mintPubkey,
+        userTokenAccount,
+        programVaultAccount,
+      })
+      .signers([user])
+      .rpc();
+
+      // Assert
+      const vaultBalance = await connection.getTokenAccountBalance(programVaultAccount);
+      expect(vaultBalance.value.amount).to.be.equal(intentAmount.toString());
+
+      const spokeState = await program.account.spokeState.fetch(spokeStateAddress);
+      expect(spokeState.status.length).to.be.equal(1);
+    });
+  });
+
+  describe('#pause', () => {
+    it('should work', async () => {
       // Sanity check
       let spokeState = await program.account.spokeState.fetch(spokeStateAddress);
       expect(spokeState.paused).to.be.equal(false);
@@ -73,8 +152,8 @@ describe("#everclear_spoke", () => {
     });
   });
 
-  describe("#unpause", () => {
-    it("should work", async () => {
+  describe('#unpause', () => {
+    it('should work', async () => {
       // Sanity check
       let spokeState = await program.account.spokeState.fetch(spokeStateAddress);
       expect(spokeState.paused).to.be.equal(true);
@@ -94,32 +173,8 @@ describe("#everclear_spoke", () => {
     });
   });
 
-  describe("#update_gateway", () => {
-    it("should work", async () => {
-      // Arrange
-      const newGatewayKeyPair = anchor.web3.Keypair.generate();
-
-      // Sanity check
-      let spokeState = await program.account.spokeState.fetch(spokeStateAddress);
-      expect(spokeState.gateway.toBase58()).to.be.equal(gatewayKeyPair.publicKey.toBase58());
-
-      // Act
-      await program.methods.updateGateway(newGatewayKeyPair.publicKey)
-        .accounts({
-          spokeState: spokeStateAddress,
-          admin: ownerKeyPair.publicKey,
-        })
-        .signers([ownerKeyPair])
-        .rpc();
-
-      // Assert
-      spokeState = await program.account.spokeState.fetch(spokeStateAddress);
-      expect(spokeState.gateway.toBase58()).to.be.equal(newGatewayKeyPair.publicKey.toBase58());
-    });
-  });
-
-  describe("#update_lighthouse", () => {
-    it("should work", async () => {
+  describe('#update_lighthouse', () => {
+    it('should work', async () => {
       // Arrange
       const newLighthouseKeyPair = anchor.web3.Keypair.generate();
 
@@ -142,8 +197,8 @@ describe("#everclear_spoke", () => {
     });
   });
 
-  describe("#update_watchtower", () => {
-    it("should work", async () => {
+  describe('#update_watchtower', () => {
+    it('should work', async () => {
       // Arrange
       const newWatchtowerKeyPair = anchor.web3.Keypair.generate();
 
@@ -166,8 +221,8 @@ describe("#everclear_spoke", () => {
     });
   });
 
-  describe("#update_mailbox", () => {
-    it("should work", async () => {
+  describe('#update_mailbox', () => {
+    it('should work', async () => {
       // Arrange
       const newMailboxKeyPair = anchor.web3.Keypair.generate();
 
@@ -190,8 +245,8 @@ describe("#everclear_spoke", () => {
     });
   });
 
-  describe("#update_message_gas_limit", () => {
-    it("should work", async () => {
+  describe('#update_message_gas_limit', () => {
+    it('should work', async () => {
       // Arrange
       const newMessageGasLimit = new anchor.BN(20000);
 
