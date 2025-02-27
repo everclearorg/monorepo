5940df9a64d3c63d9ec82cc97bec1ec81207b9cf
Layne Haber <layne.haber@gmail.com>
2025-02-27 16:20:52 -0700

    feat: fee adapter implementation

---
+++ packages/contracts/src/contracts/intent/FeeAdapter.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { SafeERC20 } from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import { Address } from '@openzeppelin/contracts/utils/Address.sol';
+import { Ownable2Step, Ownable } from '@openzeppelin/contracts/access/Ownable2Step.sol';
+
+import { TypeCasts } from 'contracts/common/TypeCasts.sol';
+
+import { IEverclear } from 'interfaces/common/IEverclear.sol';
+import { IFeeAdapter } from 'interfaces/intent/IFeeAdapter.sol';
+import { IEverclearSpoke } from 'interfaces/intent/IEverclearSpoke.sol';
+import { ISpokeGateway } from 'interfaces/intent/ISpokeGateway.sol';
+
+contract FeeAdapter is IFeeAdapter, Ownable2Step {
+  ////////////////////
+  //// Libraries /////
+  ////////////////////
+  using SafeERC20 for IERC20;
+  using TypeCasts for address;
+  using TypeCasts for bytes32;
+
+  ////////////////////
+  ///// Storage //////
+  ////////////////////
+
+  /// @inheritdoc IFeeAdapter
+  IEverclearSpoke public spoke;
+
+  /// @inheritdoc IFeeAdapter
+  address public feeRecipient;
+
+  /// @inheritdoc IFeeAdapter
+  mapping(address => uint256) public approvals;
+
+  ////////////////////
+  /// Constructor ////
+  ////////////////////
+  constructor(address _spoke, address _feeRecipient, address _owner) Ownable(_owner) {
+    spoke = IEverclearSpoke(_spoke);
+    _updateFeeRecipient(_feeRecipient);
+  }
+
+  ////////////////////
+  ////// Admin ///////
+  ////////////////////
+
+  /// @inheritdoc IFeeAdapter
+  function updateFeeRecipient(address _feeRecipient) external onlyOwner {
+    _updateFeeRecipient(_feeRecipient);
+  }
+
+  ////////////////////
+  ///// External /////
+  ////////////////////
+
+  /// @inheritdoc IFeeAdapter
+  function newIntent(
+    uint32[] memory _destinations,
+    address _receiver,
+    address _inputAsset,
+    address _outputAsset,
+    uint256 _amount,
+    uint24 _maxFee,
+    uint48 _ttl,
+    bytes calldata _data,
+    uint256 _fee
+  ) external payable returns (bytes32 _intentId, IEverclear.Intent memory _intent) {
+    // Transfer from caller
+    _pullTokens(msg.sender, _inputAsset, _amount + _fee);
+
+    // Send fees to recipient
+    _handleFees(_fee, msg.value, _inputAsset);
+
+    // Approve the spoke contract if needed
+    _approveSpokeIfNeeded(_inputAsset, _amount);
+
+    // Create new intent
+    (bytes32 _intentId, IEverclear.Intent memory _intent) = spoke.newIntent(
+      _destinations,
+      _receiver,
+      _inputAsset,
+      _outputAsset,
+      _amount,
+      _maxFee,
+      _ttl,
+      _data
+    );
+
+    // Emit event
+    emit IntentWithFeesAdded(_intentId, msg.sender.toBytes32(), _fee, msg.value);
+    return (_intentId, _intent);
+  }
+
+  ////////////////////
+  ///// Internal /////
+  ////////////////////
+
+  /**
+   * @notice Updates the fee recipient
+   * @param _feeRecipient New recipient
+   */
+  function _updateFeeRecipient(address _feeRecipient) internal {
+    emit FeeRecipientUpdated(_feeRecipient, feeRecipient);
+    feeRecipient = _feeRecipient;
+  }
+
+  /**
+   * @notice Sends fees to recipient
+   * @param _tokenFee Amount in transacting asset to send to recipient
+   * @param _nativeFee Amount in native asset to send to recipient
+   */
+  function _handleFees(uint256 _tokenFee, uint256 _nativeFee, address _inputAsset) internal {
+    // Handle token fees if exist
+    if (_tokenFee > 0) {
+      _pushTokens(feeRecipient, _inputAsset, _tokenFee);
+    }
+
+    // Handle native tokens
+    if (_nativeFee > 0) {
+      Address.sendValue(payable(feeRecipient), _nativeFee);
+    }
+  }
+
+  /**
+   * @notice Approves the maximum uint value to the gateway.
+   * @dev Approving the max reduces gas for following intents.
+   * @param _asset Asset to approve to spoke.
+   * @param _minimum Minimum required approval budget.
+   */
+  function _approveSpokeIfNeeded(address _asset, uint256 _minimum) internal {
+    // Approve the spoke contract if needed
+    if (approvals[_asset] >= _minimum) {
+      return;
+    }
+
+    // Approve to 0
+    IERC20(_asset).safeDecreaseAllowance(address(spoke), 0);
+
+    // Approve to max
+    IERC20(_asset).safeIncreaseAllowance(address(spoke), type(uint256).max);
+  }
+
+  /**
+   * @notice Pull tokens from the sender to the contract
+   * @param _sender The address of the sender
+   * @param _asset The address of the asset
+   * @param _amount The amount of the asset
+   */
+  function _pullTokens(address _sender, address _asset, uint256 _amount) internal {
+    IERC20(_asset).safeTransferFrom(_sender, address(this), _amount);
+  }
+
+  /**
+   * @notice Push tokens from the contract to the recipient
+   * @param _recipient The address of the recipient
+   * @param _asset The address of the asset
+   * @param _amount The amount of the asset
+   */
+  function _pushTokens(address _recipient, address _asset, uint256 _amount) internal {
+    IERC20(_asset).safeTransfer(_recipient, _amount);
+  }
+}
---
+++ packages/contracts/src/interfaces/intent/IFeeAdapter.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import { IEverclear } from '../common/IEverclear.sol';
+import { IEverclearSpoke } from './IEverclearSpoke.sol';
+import { ISpokeGateway } from './ISpokeGateway.sol';
+
+interface IFeeAdapter {
+  /**
+   * @notice Emitted when a new intent is created with fees
+   * @param _intentId The ID of the created intent
+   * @param _initiator The address of the user who initiated the intent
+   * @param _tokenFee The amount of token fees paid
+   * @param _nativeFee The amount of native token fees paid
+   */
+  event IntentWithFeesAdded(
+    bytes32 indexed _intentId,
+    bytes32 indexed _initiator,
+    uint256 _tokenFee,
+    uint256 _nativeFee
+  );
+
+  /**
+   * @notice Emitted when the fee recipient is updated
+   * @param _updated The new fee recipient address
+   * @param _previous The previous fee recipient address
+   */
+  event FeeRecipientUpdated(address indexed _updated, address indexed _previous);
+
+  /**
+   * @notice Returns the spoke contract address
+   * @return The EverclearSpoke contract interface
+   */
+  function spoke() external view returns (IEverclearSpoke);
+
+  /**
+   * @notice Returns the current fee recipient address
+   * @return The address that receives fees
+   */
+  function feeRecipient() external view returns (address);
+
+  /**
+   * @notice Returns the approval amount for a specific asset
+   * @param _asset The token address to check approvals for
+   * @return The amount approved to the spoke contract
+   */
+  function approvals(address _asset) external view returns (uint256);
+
+  /**
+   * @notice Creates a new intent with fees
+   * @param _destinations Array of destination domains, preference ordered
+   * @param _receiver Address of the receiver on the destination chain
+   * @param _inputAsset Address of the input asset
+   * @param _outputAsset Address of the output asset
+   * @param _amount Amount of input asset to use for the intent
+   * @param _maxFee Maximum fee percentage allowed for the intent
+   * @param _ttl Time-to-live for the intent in seconds
+   * @param _data Additional data for the intent
+   * @param _fee Token fee amount to be sent to the fee recipient
+   * @return _intentId The ID of the created intent
+   * @return _intent The created intent object
+   */
+  function newIntent(
+    uint32[] memory _destinations,
+    address _receiver,
+    address _inputAsset,
+    address _outputAsset,
+    uint256 _amount,
+    uint24 _maxFee,
+    uint48 _ttl,
+    bytes calldata _data,
+    uint256 _fee
+  ) external payable returns (bytes32, IEverclear.Intent memory);
+
+  /**
+   * @notice Updates the fee recipient address
+   * @dev Can only be called by the owner of the contract
+   * @param _feeRecipient The new address that will receive fees
+   */
+  function updateFeeRecipient(address _feeRecipient) external;
+}
