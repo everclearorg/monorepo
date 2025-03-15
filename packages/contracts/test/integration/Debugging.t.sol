f31cc2cb7a793a4a81e9cb2be48fbac2b15f8324
Layne Haber <layne.haber@gmail.com>
2025-03-14 22:28:20 -0600

    fix: reset debugging playground

--- packages/contracts/test/integration/Debugging.t.sol
+++ packages/contracts/test/integration/Debugging.t.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
-import { console } from 'forge-std/console.sol';
+// import {console} from 'forge-std/console.sol';
 
 // import { TypeCasts } from 'contracts/common/TypeCasts.sol';
 
-import { IEverclear } from 'interfaces/common/IEverclear.sol';
-import { IEverclearSpoke } from 'interfaces/intent/IEverclearSpoke.sol';
+// import { IEverclear } from 'interfaces/common/IEverclear.sol';
+// import { IAuctioneer } from 'interfaces/hub/IAuctioneer.sol';
 
 import { TestnetProductionEnvironment } from '../../script/TestnetProduction.sol';
 import { TestnetStagingEnvironment } from '../../script/TestnetStaging.sol';
@@ -45,39 +45,16 @@ import { TestExtended } from '../utils/TestExtended.sol';
 //   }
 // }
 
-contract TestnetProductionDebugging is TestExtended {
-  function test_hubMessage() public {
-    vm.createSelectFork('https://zircuit-mainnet.drpc.org');
-    address _to = address(0xD0E86F280D26Be67A672d1bFC9bB70500adA76fe);
-    address _from = address(0x2c4Baf658254C03222AEd94C91E47342669829C5);
-    uint256 _value = 0.002 ether;
-    uint32[] memory _destinations = new uint32[](1);
-    _destinations[0] = uint32(1);
-    IEverclear.Intent[] memory _intents = new IEverclear.Intent[](1);
-    _intents[0] = IEverclear.Intent({
-      initiator: bytes32(0x000000000000000000000000e623934405855449f5539186889860d9da9bc3ec),
-      receiver: bytes32(0x000000000000000000000000e623934405855449f5539186889860d9da9bc3ec),
-      inputAsset: bytes32(0x0000000000000000000000009346a5043c590133fe900aec643d9622edddba57),
-      outputAsset: bytes32(0x000000000000000000000000d7d2802f6b19843ac4dfe25022771fd83b5a7464),
-      maxFee: uint24(0),
-      origin: uint32(48900),
-      nonce: uint64(117),
-      timestamp: uint48(1741963819),
-      ttl: uint48(0),
-      amount: 8026525920369203764,
-      destinations: _destinations,
-      data: hex'00'
-    });
-    require(
-      keccak256(abi.encode(_intents[0])) == bytes32(0x82bc3df115c087b598572cf8e199ec9046000b174661e53becbd9ac0d036d741),
-      'failed id0 generation'
-    );
-
-    bytes memory _calldata = abi.encodeWithSelector(IEverclearSpoke.processIntentQueue.selector, _intents);
-
-    vm.prank(_from);
-    (bool _success, bytes memory _ret) = _to.call{ value: _value }(_calldata);
-    console.logBytes(_calldata);
-    require(_success, string(_ret));
-  }
-}
+// contract TestnetProductionDebugging is TestExtended {
+//   function test_hubMessage() public {
+//     vm.createSelectFork('https://rpc.everclear.raas.gelato.cloud');
+//     address _to = address(0xa05A3380889115bf313f1Db9d5f335157Be4D816);
+//     address _from = address(0x6d2A06543D23Cc6523AE5046adD8bb60817E0a94);
+//     uint256 _value = 0.000159522263756257 ether;
+//     bytes memory _calldata = hex'99b90fcb1b125f3879d4a84843941f410b2d85af5b05e1fa45b4f4098318cd47737bfeb6';
+//     vm.prank(_from);
+//     (bool _success, bytes memory _ret) = _to.call{value: _value}(_calldata);
+//     console.logBytes(_ret);
+//     require(_success, string(_ret));
+//   }
+// }
