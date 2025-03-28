687cb949d6c065e5ce0dac16cdb0e7421f6056d8
Oleg Tsybizov <otsybizov@gmail.com>
2025-03-28 11:38:37 -0500

    feat: populate solana intents in db

--- packages/adapters/database/db/schema.sql
+++ packages/adapters/database/db/schema.sql
@@ -31,6 +31,13 @@ CREATE SCHEMA shadow;
 
 
 --
+-- Name: solana; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA solana;
+
+
+--
 -- Name: tokenomics; Type: SCHEMA; Schema: -; Owner: -
 --
 
@@ -99,6 +106,33 @@ CREATE TYPE public.queue_type AS ENUM (
     'SETTLEMENT',
     'DEPOSIT'
 );
+
+
+--
+-- Name: base58_decode(text); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.base58_decode(base58_str text) RETURNS numeric
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+	alphabet TEXT := '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+	c CHAR(1);
+	p INT;
+	res numeric;
+BEGIN
+	res := 0;
+	FOR i IN 1..char_length(base58_str) LOOP
+		c := substring(base58_str FROM i FOR 1);
+		p := position(c IN alphabet);
+		IF p = 0 THEN
+			RAISE 'Illegal base58 character ''%''', c;
+		END IF;
+		res := (res * 58) + (p - 1);
+	END LOOP;
+
+	RETURN res;
+END;$$;
 
 
 --
@@ -266,6 +300,265 @@ $$;
 
 
 --
+-- Name: parse_and_insert_cpi_event(record); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.parse_and_insert_cpi_event(rec record) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    hex_data TEXT;
+    expected_cpi_disc TEXT := 'e445a52e51cb9a1d';
+    new_intent_disc TEXT := '1263e45a565b315d';
+    settled_disc TEXT := '75cfc4aec5c80b43';
+    cpi_disc TEXT;
+    ivent_disc TEXT;
+	pos INT := 1;
+BEGIN
+    hex_data := to_hex(base58_decode(rec.data));
+
+	cpi_disc := SUBSTRING(hex_data, pos, 16);
+	pos := pos + 16;
+	IF cpi_disc != expected_cpi_disc THEN
+		RAISE WARNING 'invalid CPI discriminator %, expected %', cpi_disc, expected_cpi_disc;
+		RETURN FALSE;
+	END IF;
+
+	ivent_disc := SUBSTRING(hex_data, pos, 16);
+	pos := pos + 16;
+	IF ivent_disc = new_intent_disc THEN
+		RETURN parse_and_insert_new_intent_cpi_event(hex_data, rec);
+	ELSIF ivent_disc = settled_disc THEN
+		RETURN parse_and_insert_settled_cpi_event(hex_data, rec);
+	END IF;
+
+    RETURN FALSE;
+END;$$;
+
+
+--
+-- Name: parse_and_insert_new_intent_cpi_event(text, record); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.parse_and_insert_new_intent_cpi_event(hex_data text, rec record) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    intent_id TEXT;
+    message_id TEXT;
+    initiator TEXT;
+    receiver TEXT;
+    input_asset TEXT;
+    output_asset TEXT;
+    normalized_amount BIGINT;
+    max_fee INT;
+    origin_domain INT;
+    nonce BIGINT;
+    ttl BIGINT;
+    timestamp BIGINT;
+    destination_count INT;
+    destinations VARCHAR(66)[];
+    data_length INT;
+    data TEXT;
+	pos INT := 33;
+	i INT;
+BEGIN
+	intent_id := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	message_id := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	initiator := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	receiver := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	input_asset := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	output_asset := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	normalized_amount := to_bigint(reverse_bytes(SUBSTRING(hex_data, pos, 16)));
+	pos := pos + 16;
+	max_fee := to_int(reverse_bytes(SUBSTRING(hex_data, pos, 8)));
+	pos := pos + 8;
+	origin_domain := to_int(reverse_bytes(SUBSTRING(hex_data, pos, 8)));
+	pos := pos + 8;
+	nonce := to_bigint(SUBSTRING(hex_data, pos, 16));
+	pos := pos + 16;
+	ttl := to_bigint(reverse_bytes(SUBSTRING(hex_data, pos, 16)));
+	pos := pos + 16;
+	timestamp := to_bigint(reverse_bytes(SUBSTRING(hex_data, pos, 16)));
+	pos := pos + 16;
+	destination_count := to_int(reverse_bytes(SUBSTRING(hex_data, pos, 8)));
+	pos := pos + 8;
+
+
+	FOR i IN 0..(destination_count - 1) LOOP
+		destinations[i] := to_int(reverse_bytes(SUBSTRING(hex_data, pos, 8)));
+		pos := pos + 8;
+	END LOOP;
+
+	data_length := to_int(reverse_bytes(SUBSTRING(hex_data, pos, 8)));
+	pos := pos + 8;
+
+	data := '0x' || SUBSTRING(hex_data, pos, data_length);
+
+	INSERT INTO public.origin_intents(
+		id,
+		queue_idx,
+		message_id,
+		receiver,
+		input_asset,
+		output_asset,
+		amount,
+		max_fee,
+		origin,
+		nonce,
+		data,
+		transaction_hash,
+		"timestamp",
+		block_number,
+		tx_origin,
+		tx_nonce,
+		auto_id,
+		gas_limit,
+		gas_price,
+		status,
+		initiator,
+		ttl,
+		destinations
+	)
+	VALUES (
+		intent_id,
+		0,
+		message_id,
+		receiver,
+		input_asset,
+		output_asset,
+		normalized_amount,
+		max_fee,
+		origin_domain,
+		nonce,
+		data,
+		'',
+		timestamp,
+		rec.block_slot,
+		'',
+		0,
+		0,
+		rec.tx_fee,
+		1,
+		'DISPATCHED',
+		initiator,
+		ttl,
+		destinations
+	);
+
+    RETURN TRUE;
+END;$$;
+
+
+--
+-- Name: parse_and_insert_settled_cpi_event(text, record); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.parse_and_insert_settled_cpi_event(hex_data text, rec record) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    intent_id TEXT;
+    recipient TEXT;
+    asset TEXT;
+    amount BIGINT;
+    domain INT;
+	pos INT := 33;
+BEGIN
+	intent_id := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	recipient := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	asset := '0x' || SUBSTRING(hex_data, pos, 64);
+	pos := pos + 64;
+	amount := to_bigint(reverse_bytes(SUBSTRING(hex_data, pos, 16)));
+	pos := pos + 16;
+	domain := to_int(reverse_bytes(SUBSTRING(hex_data, pos, 8)));
+	pos := pos + 8;
+
+	INSERT INTO public.settlement_intents(
+		id,
+		amount,
+		asset,
+		recipient,
+		domain,
+		transaction_hash,
+		"timestamp",
+		block_number,
+		tx_origin,
+		tx_nonce,
+		auto_id,
+		gas_limit,
+		gas_price,
+		return_data,
+		status
+	)
+	VALUES (
+		intent_id,
+		amount,
+		asset,
+		recipient,
+		domain,
+		'',
+		rec.block_timestamp,
+		rec.block_slot,
+		'',
+		0,
+		0,
+		rec.tx_fee,
+		1,
+		'0x',
+		'SETTLED'
+	);
+
+    RETURN TRUE;
+END;$$;
+
+
+--
+-- Name: process_cpi_events(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.process_cpi_events() RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    rec record;
+	res BOOLEAN;BEGIN
+    FOR rec IN (SELECT id, block_slot, block_timestamp, tx_signature, tx_fee, data
+		FROM solana.solana_spoke_instructions WHERE accounts = '["HoUvmo3eC8gwMknYvyhto8S8iT8xZryUdErfXhawoHeG"]'
+		AND tx_status = 1 AND tx_err = 'null' AND processed = FALSE) LOOP
+			res := parse_and_insert_cpi_event(rec);
+	        UPDATE solana.solana_spoke_instructions SET processed = TRUE WHERE id = rec.id;
+    END LOOP;
+END;$$;
+
+
+--
+-- Name: reverse_bytes(text); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.reverse_bytes(hex_str text) RETURNS text
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    reversed TEXT := '';
+BEGIN
+    FOR i IN 0..(length(hex_str) / 2 - 1) LOOP
+        reversed := substr(hex_str, i * 2 + 1, 2) || reversed;
+    END LOOP;
+    RETURN reversed;
+END;
+$$;
+
+
+--
 -- Name: table_exists(text, text); Type: FUNCTION; Schema: public; Owner: -
 --
 
@@ -279,6 +572,52 @@ BEGIN
         WHERE table_schema = ''%s'' AND table_name LIKE ''%s''', schema_name, table_name) INTO table_count;
 
     return table_count > 0;
+END;
+$$;
+
+
+--
+-- Name: to_bigint(text); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.to_bigint(hex_str text) RETURNS bigint
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    RETURN CAST(CAST(('x' || hex_str) AS bit(64)) AS BIGINT);
+END;
+$$;
+
+
+--
+-- Name: to_hex(numeric); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.to_hex(n numeric) RETURNS text
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    b INT;
+    res TEXT := '';
+BEGIN
+    WHILE n > 0 LOOP
+        b := n % 256;
+        res := lpad(to_hex(b), 2, '0') || res;
+        n := (n - b) / 256;
+    END LOOP;
+    RETURN res;
+END;$$;
+
+
+--
+-- Name: to_int(text); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.to_int(hex_str text) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    RETURN CAST(CAST(('x' || hex_str) AS bit(32)) AS INT);
 END;
 $$;
 
@@ -3720,6 +4059,33 @@ CREATE TABLE tokenomics.withdraw_eth (
 
 
 --
+-- Name: solana_spoke_instructions; Type: TABLE; Schema: solana; Owner: -
+--
+
+CREATE TABLE solana.solana_spoke_instructions (
+    id text NOT NULL,
+    block_slot bigint,
+    block_hash text,
+    block_timestamp bigint,
+    tx_signature text,
+    tx_status bigint,
+    tx_index bigint,
+    tx_fee bigint,
+    tx_err text,
+    index bigint,
+    parent_index bigint,
+    accounts text,
+    data text,
+    program text,
+    program_id text,
+    instruction_type text,
+    params text,
+    parsed text,
+    processed boolean DEFAULT false NOT NULL
+);
+
+
+--
 -- Name: destination_intents auto_id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -5625,4 +5991,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20241206140725'),
     ('20241217225034'),
     ('20250107062058'),
-    ('20250108140315');
+    ('20250108140315'),
+    ('20250321152002');
