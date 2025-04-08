a9f9df7349510351420334541c20138c08f7162a
Layne Haber <layne.haber@gmail.com>
2025-04-08 17:17:19 -0600

    fix: empty fees

--- packages/contracts/script/assets/mainnetproduction/USDC.s.sol
+++ packages/contracts/script/assets/mainnetproduction/USDC.s.sol
@@ -29,8 +29,7 @@ contract USDC is AddAssetBase, MainnetProductionEnvironment {
                               TOKEN FEES 
     //////////////////////////////////////////////////////////////*/
 
-    IHubStorage.Fee[] memory _fees = new IHubStorage.Fee[](1);
-    _fees[0] = IHubStorage.Fee({recipient: FEE_RECIPIENT, fee: 0}); // 0.2 BPS
+    IHubStorage.Fee[] memory _fees = new IHubStorage.Fee[](0);
 
     /*///////////////////////////////////////////////////////////////
                          ADOPTED CONFIGURATION  
