7ebcaa7c6de0fc8c7a027daa6368c31ac43575cc
Harry Lee Chak Chiu <harrier1126@gmail.com>
2025-04-14 00:23:46 +0200

    chore: fix lint

--- packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/receive_message/admin.rs
+++ packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/receive_message/admin.rs
@@ -7,7 +7,7 @@ use super::HandleContext;
 /// Receive a cross‑chain message via Hyperlane via admin.
 /// This have the same interface as the hyperlane message `handle`, and should only be used if hyperlane fails
 /// to transmit message.
-pub fn handle_as_admin<'info>(
+pub fn handle_as_admin(
     ctx: Context<HandleContext>,
     handle: HandleInstruction,
 ) -> Result<()> {
