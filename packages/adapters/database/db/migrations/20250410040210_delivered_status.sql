d4afeadfdf92e66fb6fc9562e11cc0300e0ca74b
Oleg Tsybizov <otsybizov@gmail.com>
2025-04-14 14:07:00 -0500

    fix: fee adapter and solana db migration issues

--- packages/adapters/database/db/migrations/20250410040210_delivered_status.sql
+++ packages/adapters/database/db/migrations/20250410040210_delivered_status.sql
@@ -1,5 +1,5 @@
 -- migrate:up
 -- Add 'DELIVERED' to intent_status enum
-ALTER TYPE public.intent_status ADD VALUE 'DELIVERED';
+ALTER TYPE public.intent_status ADD VALUE IF NOT EXISTS 'DELIVERED';
 
 -- migrate:down
