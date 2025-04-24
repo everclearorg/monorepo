9c0a8a28b2a129da9cc219d1eeb6394f51606d8f
just-a-node <eye1717@gmail.com>
2025-04-24 13:04:22 -0600

    test: fix testnet expected gelato addr

--- packages/utils/test/peripherals/gelato.spec.ts
+++ packages/utils/test/peripherals/gelato.spec.ts
@@ -4,7 +4,7 @@ describe('Peripherals:Gelato', () => {
   describe('#getGelatoRelayerAddress', () => {
     it('happy', () => {
       // zkSync networks
-      expect(getGelatoRelayerAddress('280')).to.be.eq('0x0c1B63765Be752F07147ACb80a7817A8b74d9831');
+      expect(getGelatoRelayerAddress('280')).to.be.eq('0x30532F63B02c5bBb6D6f684Cbc7bebfC5deF407B');
       expect(getGelatoRelayerAddress('324')).to.be.eq('0x30532F63B02c5bBb6D6f684Cbc7bebfC5deF407B');
 
       // Other networks
