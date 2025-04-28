c667dfb8e0501d6f0bd71f92de4686c9672e06f7
Harry Lee Chak Chiu <harrier1126@gmail.com>
2025-04-28 11:27:01 -0400

    fix: bump amazon rabbitmq version to 3.13 as 3.12 is out of support

--- ops/modules/amq/main.tf
+++ ops/modules/amq/main.tf
@@ -2,7 +2,7 @@ resource "aws_mq_broker" "default" {
   broker_name         = "rmq-cluster-${var.environment}-${var.stage}"
   deployment_mode     = var.deployment_mode
   engine_type         = "RabbitMQ"
-  engine_version      = "3.12.13"
+  engine_version      = "3.13"
   host_instance_type  = var.host_instance_type
   publicly_accessible = var.publicly_accessible
   subnet_ids          = var.publicly_accessible ? [] : var.subnet_ids
@@ -29,6 +29,3 @@ resource "aws_mq_broker" "default" {
     password = var.rmq_mgt_password
   }
 }
-
-
-
