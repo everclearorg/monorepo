fe268663799f55939aecac043a213145b7aaeed7
Prathmesh <201952225@iiitvadodara.ac.in>
2025-05-07 20:24:49 +0530

    fix: new_order method

--- packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/fee_adapter/new_order.rs
+++ packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/fee_adapter/new_order.rs
@@ -3,10 +3,10 @@ use anchor_lang::prelude::*;
 use crate::consts::DEFAULT_NORMALIZED_DECIMALS;
 use crate::error::SpokeError;
 use crate::events::OrderCreated;
-use crate::instructions::{new_intent, NewIntent};
+use crate::instructions::{handle_new_intent, NewIntent, NewIntentAccounts};
 use crate::intent::{u128_to_u256_be, EVMIntent};
 use crate::types::OrderParameters;
-use crate::utils::{compute_intent_hash, normalize_decimals};
+use crate::utils::{hash_intent_id_array,compute_intent_hash ,normalize_decimals};
 
 /// Batch-create multiple intents and handle fees.
 pub fn new_order(
@@ -25,13 +25,35 @@ pub fn new_order(
 
     // handle_fees(&ctx, fee, asset, deadline, sig)?; yet to implement
 
-    let accounts = &ctx.accounts;
-    let state = &mut accounts.spoke_state;
+
+    let mut accounts = NewIntentAccounts {
+        spoke_state: ctx.accounts.spoke_state.clone(),
+        mint: ctx.accounts.mint.clone(),
+        token_program: ctx.accounts.token_program.clone(),
+        program_vault_account: ctx.accounts.program_vault_account.clone(),
+        user_token_account: ctx.accounts.user_token_account.clone(),
+        authority: ctx.accounts.authority.clone(),
+        system_program: ctx.accounts.system_program.clone(),
+        spl_noop_program: ctx.accounts.spl_noop_program.clone(),
+        hyperlane_mailbox: ctx.accounts.hyperlane_mailbox.clone(),
+        mailbox_outbox: ctx.accounts.mailbox_outbox.clone(),
+        dispatch_authority: ctx.accounts.dispatch_authority.clone(),
+        unique_message_account: ctx.accounts.unique_message_account.clone(),
+        dispatched_message_pda: ctx.accounts.dispatched_message_pda.clone(),
+        igp_program: ctx.accounts.igp_program.clone(),
+        igp_program_data: ctx.accounts.igp_program_data.clone(),
+        igp_payment_pda: ctx.accounts.igp_payment_pda.clone(),
+        configured_igp_account: ctx.accounts.configured_igp_account.clone(),
+        inner_igp_account: ctx.accounts.inner_igp_account.clone(),
+    };
+
+    let program_id = ctx.program_id.clone();
 
     let mut intent_ids: Vec<[u8; 32]> = Vec::with_capacity(params.len());
     for p in &params {
-        new_intent(
-            ctx.clone(),
+        let _ = handle_new_intent(
+            &mut accounts,
+            program_id,
             p.receiver,
             p.input_asset,
             p.output_asset,
@@ -41,7 +63,7 @@ pub fn new_order(
             p.destinations.clone(),
             p.data.clone(),
             p.message_gas_limit,
-        )?;
+        );
 
         // Recompute intent ID exactly as in new_intent
         let minted_decimals = ctx.accounts.mint.decimals;
@@ -70,11 +92,11 @@ pub fn new_order(
     }
 
     // 4) Derive order ID and emit event
-    let order_id =
-        compute_intent_hash(&intent_ids.iter().map(|id| id.as_ref()).collect::<Vec<_>>());
-    emit!(OrderCreated {
+    let order_id = hash_intent_id_array(&intent_ids);
+
+    emit_cpi!(OrderCreated {
         order_id,
-        user: ctx.accounts.authority.key().to_bytes(),
+        user: ctx.accounts.authority.key(),
         intent_ids: intent_ids.clone(),
         fee,
         native_value: ctx.accounts.authority.lamports(),
