bf630d3e4cbef6cd39cc98ba5df7b806ffc92150
Harry Lee Chak Chiu <harrier1126@gmail.com>
2025-05-29 03:27:16 -0400

    feat: setup postgrest jwt secret for api role authentication

--- ops/mainnet/prod/backend/config.tf
+++ ops/mainnet/prod/backend/config.tf
@@ -22,6 +22,7 @@ locals {
     { name = "PGRST_DB_URI", value = "postgres://${var.postgres_user}:${var.postgres_password}@${module.cartographer_db.db_instance_endpoint}/everclear" },
     { name = "PGRST_DB_SCHEMA", value = "public" },
     { name = "PGRST_DB_ANON_ROLE", value = "query" },
+    { name = "PGRST_JWT_SECRET", value = "${var.postgrest_jwt_secret}"},
     { name = "ENVIRONMENT", value = var.environment },
     { name = "STAGE", value = var.stage },
     { name = "PGRST_DB_AGGREGATES_ENABLED", value = "true" }
