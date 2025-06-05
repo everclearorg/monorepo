f068055f26338edeaf56424fd6cd7e42e92c8d2e
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-06-05 10:57:32 +0300

    Update errors.ts

--- packages/adapters/chainservice/src/shared/errors.ts
+++ packages/adapters/chainservice/src/shared/errors.ts
@@ -383,7 +383,7 @@ export class TransactionProcessingError extends EverclearError {
   static readonly reasons = {
     SubmitOutOfOrder: 'Submit was called but transaction is already completed.',
     MineOutOfOrder: 'Transaction mine or confirm was called, but no transaction has been sent.',
-    ConfirmOutOfOrder: "Tried to confirm but tansaction did not complete 'mine' step; no receipt was found.",
+    ConfirmOutOfOrder: "Tried to confirm but transaction did not complete 'mine' step; no receipt was found.",
     DuplicateHash: 'Received a transaction response with a duplicate hash!',
     NoReceipt: 'No receipt was returned from the transaction.',
     NullReceipt: 'Unable to obtain receipt: ethers responded with null.',
