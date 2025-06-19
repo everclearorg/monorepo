b523f4963d6b3f7a2e59b474caac3174ac64906f
Arpit Bharti <arpitbharti73@gmail.com>
2025-06-19 20:02:36 +0530

    feat: update table schema

--- packages/adapters/database/db/migrations/20250522084000_otc_table.sql
+++ packages/adapters/database/db/migrations/20250522084000_otc_table.sql
@@ -1,14 +1,16 @@
 -- migrate:up
 
 CREATE TABLE public.otc_sale_table (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),     -- unique ID per entry
-  partner_id TEXT NOT NULL,                          -- identifier for the partner
-  origin INTEGER NOT NULL,                           -- origin domain_id
-  destination INTEGER[] NOT NULL,                       -- destination domain_id
-  token TEXT NOT NULL,                               -- ticker hash (e.g. bytes32 hex string)
-  amount TEXT NOT NULL,                              -- decimal-formatted string (stored as TEXT)
-  total_fee TEXT NOT NULL,                           -- also stored as TEXT, supports large decimals
-  created_at TIMESTAMP DEFAULT NOW()                 -- optional timestamp
+  order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),    -- unique ID per entry
+  partner_id TEXT NOT NULL,                               -- identifier for the partner
+  origin INTEGER NOT NULL,                                -- origin domain_id
+  destinations INTEGER[] NOT NULL,                        -- destinations domain_id
+  ticker_hash TEXT NOT NULL,                              -- ticker hash (e.g. bytes32 hex string)
+  amount TEXT NOT NULL,                                   -- decimal-formatted string (stored as TEXT)
+  total_fee TEXT NOT NULL,                                -- also stored as TEXT, supports large decimals
+  transaction_hash TEXT NULL,                             -- transaction hash of intent created for the order
+  created_at TIMESTAMP DEFAULT NOW(),                     -- optional timestamp
+  expires_at TIMESTAMP NOT NULL                           -- optional timestamp
 );
 
 -- migrate:down
