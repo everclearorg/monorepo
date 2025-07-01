d74487f490998ca9588c8259de74bef16d01aca0
0xHarbs <jamesharbridge@outlook.com>
2025-07-01 09:00:17 +0200

    chore: revert temporary changes

--- packages/contracts/test/unit/upgrades/swaps/SpokeUpgradeSwaps.t.sol
+++ packages/contracts/test/unit/upgrades/swaps/SpokeUpgradeSwaps.t.sol
@@ -187,7 +187,6 @@ contract SpokeUpgradeSwaps is BaseTest, UpgradeHelper {
    * @notice Tests the deposit function of the spoke proxy
    * @dev This function is used to deposit tokens into the spoke proxy
    */
-
   function test_spokeUpgradeSwaps_deposit() public {
     _upgradeSpoke();
 
