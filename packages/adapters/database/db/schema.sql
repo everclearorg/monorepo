59b7bc849675c06b3b869f85b453766cdc599e19
Oleg Tsybizov <otsybizov@gmail.com>
2025-07-08 13:28:40 -0500

    fix: tron event handling

--- packages/adapters/database/db/schema.sql
+++ packages/adapters/database/db/schema.sql
@@ -1353,35 +1353,6 @@ END;$$;
 
 
 --
--- Name: process_tron_gateway_events(); Type: FUNCTION; Schema: public; Owner: -
---
-
-CREATE FUNCTION public.process_tron_gateway_events() RETURNS trigger
-    LANGUAGE plpgsql SECURITY DEFINER
-    SET search_path TO 'tron', 'public'
-    AS $$
-DECLARE
-    res BOOLEAN;
-BEGIN
-    -- IntentQueueProcessed event
-    IF NEW.topics LIKE '0x43a52e9a77f317a192970b363b14ece56df243fe0dd94f459f63029d657efec3%' THEN
-        res := add_new_tron_intent_message(NEW);
-        IF res IS FALSE THEN
-            RAISE WARNING 'Failed to parse and insert new tron intent message, transaction %', NEW.transaction_hash;
-        END IF;
-    -- FillQueueProcessed event
-    ELSIF NEW.topics LIKE '0x5e3a5b80dcf8e0fb984fe128ed0db507a86cc0674c4f5980f83b129b2cfdc69e%' THEN
-        res := add_new_tron_fill_message(NEW);
-        IF res IS FALSE THEN
-            RAISE WARNING 'Failed to parse and insert new tron fill message, transaction %', NEW.transaction_hash;
-        END IF;
-    END IF;
-
-    RETURN NEW;
-END;$$;
-
-
---
 -- Name: process_tron_spoke_events(); Type: FUNCTION; Schema: public; Owner: -
 --
 
@@ -1409,6 +1380,18 @@ BEGIN
         res := add_new_tron_settlement(NEW);
         IF res IS FALSE THEN
             RAISE WARNING 'Failed to parse and insert new tron settlement, transaction %', NEW.transaction_hash;
+        END IF;
+    -- IntentQueueProcessed event
+    ELSIF NEW.topics LIKE '0x43a52e9a77f317a192970b363b14ece56df243fe0dd94f459f63029d657efec3%' THEN
+        res := add_new_tron_intent_message(NEW);
+        IF res IS FALSE THEN
+            RAISE WARNING 'Failed to parse and insert new tron intent message, transaction %', NEW.transaction_hash;
+        END IF;
+    -- FillQueueProcessed event
+    ELSIF NEW.topics LIKE '0x5e3a5b80dcf8e0fb984fe128ed0db507a86cc0674c4f5980f83b129b2cfdc69e%' THEN
+        res := add_new_tron_fill_message(NEW);
+        IF res IS FALSE THEN
+            RAISE WARNING 'Failed to parse and insert new tron fill message, transaction %', NEW.transaction_hash;
         END IF;
     END IF;
 
@@ -4432,13 +4415,6 @@ CREATE TRIGGER process_cpi_events_trigger BEFORE INSERT OR UPDATE ON solana.sola
 
 
 --
--- Name: tron_gateway_raw_logs process_tron_gateway_events_trigger; Type: TRIGGER; Schema: tron; Owner: -
---
-
-CREATE TRIGGER process_tron_gateway_events_trigger BEFORE INSERT OR UPDATE ON tron.tron_gateway_raw_logs FOR EACH ROW EXECUTE FUNCTION public.process_tron_gateway_events();
-
-
---
 -- Name: tron_spoke_raw_logs process_tron_spoke_events_trigger; Type: TRIGGER; Schema: tron; Owner: -
 --
 
@@ -4571,4 +4547,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20250612163456'),
     ('20250623193039'),
     ('20250624105537'),
-    ('20250701013945');
+    ('20250701013945'),
+    ('20250708181540');
