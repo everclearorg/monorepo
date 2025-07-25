dfb562d659455515903e9fb66c5ebe60fe2f1fdf
preethamr <preethamr@users.noreply.github.com>
2025-07-25 11:34:53 -0700

    fix: handle solana rpc errors better

--- ops/mainnet/prod/backend/variables.tf
+++ ops/mainnet/prod/backend/variables.tf
@@ -109,3 +109,9 @@ variable "alchemy_key" {
   sensitive = true
   default   = "neverclear"
 }
+
+variable "quicknode_api_key" {
+  type      = string
+  sensitive = true
+  default   = "neverclear"
+}
