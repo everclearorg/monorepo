4460a5363a5b097480d3e403da6d3454cadbd23e
Harry Lee Chak Chiu <harrier1126@gmail.com>
2025-08-22 12:47:41 -0400

    Revert "fix: change priority to use everclear relayer beforte gelato"
    This reverts commit f6f8c9a306ab4d057a4b47a6258ce671a86bca6a.

--- ops/mainnet/prod/core/config.tf
+++ ops/mainnet/prod/core/config.tf
@@ -550,14 +550,14 @@ locals {
     }
     relayers = [
       {
-        type   = "Everclear",
-        apiKey = "${var.admin_token_relayer}",
-        url    = "https://${module.relayer_server.service_endpoint}"
-      },
-      {
         type   = "Gelato",
         apiKey = "${var.gelato_api_key}",
         url    = "https://relay.gelato.digital"
+      },
+      {
+        type   = "Everclear",
+        apiKey = "${var.admin_token_relayer}",
+        url    = "https://${module.relayer_server.service_endpoint}"
       }
     ]
     agents = {
@@ -602,14 +602,14 @@ locals {
     network = "mainnet"
     relayers = [
       {
-        type   = "Everclear",
-        apiKey = "${var.admin_token_relayer}",
-        url    = "https://${module.relayer_server.service_endpoint}"
-      },
-      {
         type   = "Gelato",
         apiKey = "${var.gelato_api_key}",
         url    = "https://relay.gelato.digital"
+      },
+      {
+        type   = "Everclear",
+        apiKey = "${var.admin_token_relayer}",
+        url    = "https://${module.relayer_server.service_endpoint}"
       }
     ]
     thresholds = {
