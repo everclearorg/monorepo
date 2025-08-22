8946842b6a8be08fc8038d6bc34870e4027e7953
0xHarbs <jamesharbridge@outlook.com>
2025-08-22 16:26:29 +0100

    test: wip fixing hub

--- packages/contracts/test/unit/upgrades/swaps/HubUpgradeSwaps.t.sol
+++ packages/contracts/test/unit/upgrades/swaps/HubUpgradeSwaps.t.sol
@@ -3223,6 +3223,8 @@ contract HubUpgradeSwaps is BaseTest, UpgradeHelper {
     assertEq(_maxDiscountDbps, _maxDiscount);
   }
 
+  function test_hubUpgradeSwaps_setMaxDiscountDBPS() public {}
+
   // ============ Upgrades Functions ============ //
   function test_hubUpgradeSwaps_updateModuleAddress_Settlement() public {
     _upgradeHub();
