01740b7c7555906d381061ad706d3d545f5b6e1c
0xHarbs <jamesharbridge@outlook.com>
2025-08-28 17:34:37 +0100

    test: hub tests completed

--- packages/contracts/src/contracts/hub/modules/HubMessageReceiverV2.sol
+++ packages/contracts/src/contracts/hub/modules/HubMessageReceiverV2.sol
@@ -13,7 +13,6 @@ import {IEverclearV2} from 'interfaces/common/IEverclearV2.sol';
 import {IHubMessageReceiverV2, IMessageReceiver} from 'interfaces/hub/IHubMessageReceiverV2.sol';
 
 import {SettlerLogicV2} from 'contracts/hub/modules/SettlerLogicV2.sol';
-import 'forge-std/console2.sol';
 
 /**
  * @title HubMessageReceiverV2
