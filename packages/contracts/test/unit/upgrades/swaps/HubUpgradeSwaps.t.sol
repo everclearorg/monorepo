f7143db79eb0793ce46d10cf243e9bd12b3d3f47
0xHarbs <jamesharbridge@outlook.com>
2025-08-29 14:32:46 +0100

    chore: linting

--- packages/contracts/test/unit/upgrades/swaps/HubUpgradeSwaps.t.sol
+++ packages/contracts/test/unit/upgrades/swaps/HubUpgradeSwaps.t.sol
@@ -3017,7 +3017,7 @@ contract HubUpgradeSwaps is BaseTest, UpgradeHelper {
 
   // ============ Discount Maths ============ //
   function test_hubUpgradeSwaps_invoiceNotDiscounted() public {}
-  
+
   function test_hubUpgradeSwaps_invoiceDiscountedFiveTimes() public {}
 
   // ============ Fee Maths ============ //
