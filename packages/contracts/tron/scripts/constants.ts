67727efe7d3193e8934e226a1b787b4bf500cbff
0xHarbs <jamesharbridge@outlook.com>
2025-10-08 14:12:47 +0100

    chore: updating constants

--- packages/contracts/tron/scripts/constants.ts
+++ packages/contracts/tron/scripts/constants.ts
@@ -1,6 +1,7 @@
 // Global //
-export const POLYMER_PROD_ISM: string = 'TVxZzZEK9dKj1Y5ZK4HhEFZPWGQEwDuz47'; // Prod ISM: TVxZzZEK9dKj1Y5ZK4HhEFZPWGQEwDuz47
-export const POLYMER_MAILBOX: string = '415b34081e9d453fc2ba925d893583d89d1b7175dd';
+export const POLYMER_PROD_MAILBOX: string = 'TJHSpk2YGfNWuFyDpsbjCY33o8FtDeMapv';
+export const POLYMER_PROD_ISM: string = 'TVxZzZEK9dKj1Y5ZK4HhEFZPWGQEwDuz47';
+
 export const POLYMER_STAGING_MAILBOX: string = 'TGWEpW7aytCrJhryWZ73M8G22Q4cKh5LUa';
 export const POLYMER_STAGING_ISM: string = 'TAX5oSJURLpgf4C1GAjuSAdxNt6P6WWX41';
 
@@ -8,11 +9,14 @@ export const MESSAGE_RECEIVER: string = '4147120c330314b3929b3efa22234a069f84e2c
 export const CALL_EXECUTOR: string = '41902fdfc8e489100aadfc982d7c5144253b5dfb81';
 export const GATEWAY_IMPL: string = '417039676630aba9606afa13bfb4b822d67c05282a';
 export const SPOKE_IMPL: string = 'TRooMrhE5VP2JFRyBf74fqMijMGx8usXuX';
+
 export const FEE_SIGNER: string = 'TPRnuwo64F7ndhEKhWByyPs7BgNMpzQwuP';
 export const FEE_SIGNER_PROD: string = 'THFQDwfJJw4GSMdt4X7z2wRWDH7gGVNSB6';
+
 export const tronLighthouse: string = 'TFAJBqyUQmudXW1fanNN4a5V76qEDTouT7';
 export const tronWatchtower: string = 'TJx4rVn6Nf2P1ZL9m5G737EYLD3BQdF7rj';
 export const tronOwner: string = 'TATCzhQqxq9DRppGHiEFvFuoDW6tHaESqg'; // NOTE: Using EOA to enable the update of the gateway
+
 export const tronMaxSolversFee: number = 5000;
 export const hubDomain: number = 25327;
 export const tronIsm: string = POLYMER_STAGING_ISM;
@@ -23,8 +27,8 @@ export const EVERCLEAR_SPOKE_IMPL = 'TRooMrhE5VP2JFRyBf74fqMijMGx8usXuX';
 export const EVERCLEAR_SPOKE_GATEWAY_IMPL = '417039676630aba9606afa13bfb4b822d67c05282a'; // TLCbT376siRg4PvzGBXYq4a1xafWJyzM5n || 417039676630aba9606afa13bfb4b822d67c05282a
  
 // Production //
-export const SPOKE_PROD: string = '419b266df36c882a73d45b18876104d5728424828f'; // TQ7ZmvN6K2TaSpzbz8KUw8bSK2zsSeksxJ
-export const EVERCLEAR_SPOKE_GATEWAY_PROD = '418fd8a4d1980fa73f060a37af5bf23d8fb2b68a0b';
+export const SPOKE_PROD: string = 'TQ7ZmvN6K2TaSpzbz8KUw8bSK2zsSeksxJ';
+export const EVERCLEAR_SPOKE_GATEWAY_PROD = 'TP5oAAfdfNbAF8VMhTfCw3uQjpQii5pZWH';
 export const hubGatewayProd: string = '41EFfAB7cCEBF63FbEFB4884964b12259d4374FaAa';
 export const HUB_GATEWAY_PROD = '0x000000000000000000000000effab7ccebf63fbefb4884964b12259d4374faaa';
 export const XERC20_MODULE_PROD: string = '41af50e223f96be03403e6a2535a2e4508bca1d778';
@@ -35,11 +39,12 @@ export const EVERCLEAR_SPOKE_IMPL_V5: string = 'TVNcMDJBV8V9mZaPqbxaj29QSiR4u7sM
 
 // Staging //
 export const SPOKE_STAGING: string = 'TVgfN2ewsCKmFd4NkP833NQzeFtZRXvp1g';
+export const EVERCLEAR_SPOKE_GATEWAY_STAGING = 'TCqgpCHXGiVbPnEafTK6JGaNjaZFAG3m7Y';
 export const hubGatewayStaging: string = '41e5f2f4afad6211cfbd6a882d5a6a435530ee3909'; // 0xe5F2F4afAd6211cfBD6a882D5a6a435530Ee3909
 export const HUB_GATEWAY_STAGING = '0x000000000000000000000000e5f2f4afad6211cfbd6a882d5a6a435530ee3909';
 export const XERC20_MODULE_STAGING: string = '419239e1c157d6dc7d6249b37512455c5a489a6cd7';
 export const FEE_ADAPTER_STAGING: string = 'TWycgYzTWmDfrUaTikcmFGJpfZRjQXPXyF';
-export const EVERCLEAR_SPOKE_GATEWAY_STAGING = '411f7c443b1793e2223541ee90814fe2a1f8b8778f';
+
 
 // Helper //
 export function fetchAddresses(logProd: boolean) {
--- packages/contracts/tron/scripts/readSpokeState.ts
+++ packages/contracts/tron/scripts/readSpokeState.ts
@@ -23,78 +23,78 @@ const tronWeb = new TronWeb.TronWeb({
 
     // Construct spoke (proxy) state
     const spokeInstance = await tronWeb.contract(EverclearSpokeV5Artifact.abi, spokeAddress);
-    console.log('-- Spoke address:', spokeInstance.address);
+    console.log('-- Spoke address:', tronWeb.address.fromHex(spokeInstance.address));
 
     // Construct gateway (proxy) state
     const gatewayInstance = await tronWeb.contract(SpokeGatewayArtifact.abi, gatewayAddress);
-    console.log('-- Gateway address:', gatewayInstance.address);
+    console.log('-- Gateway address:', tronWeb.address.fromHex(gatewayInstance.address));
 
     // XERC20 module //
     const xerc20Instance = await tronWeb.contract(XERC20ModuleArtifact.abi, xerc20Module);
-    console.log('XERC20 module address:', xerc20Instance.address);
+    console.log('XERC20 module address:', tronWeb.address.fromHex(xerc20Instance.address));
 
     // Fee adapter //
     const feeAdapterInstance = await tronWeb.contract(FeeAdapterArtifact.abi, feeAdapter);
-    console.log('Fee Adapter address:', feeAdapterInstance.address);
+    console.log('Fee Adapter address:', tronWeb.address.fromHex(feeAdapterInstance.address));
 
     // Reading the Spoke instance state //
     const spokeOwner = await spokeInstance.owner().call();
-    console.log('Owner of the Spoke:', spokeOwner);
+    console.log('Owner of the Spoke:', tronWeb.address.fromHex(spokeOwner));
 
     const spokeGateway = await spokeInstance.gateway().call();
-    console.log('Gateway address:', spokeGateway);
+    console.log('Gateway address:', tronWeb.address.fromHex(spokeGateway));
 
     // Reading the messageReceiver and callExecutor //
     const messageReceiver = await spokeInstance.messageReceiver().call();
-    console.log('Message Receiver address:', messageReceiver);
+    console.log('Message Receiver address:', tronWeb.address.fromHex(messageReceiver));
 
     const callExecutor = await spokeInstance.callExecutor().call();
-    console.log('Call Executor address:', callExecutor);
+    console.log('Call Executor address:', tronWeb.address.fromHex(callExecutor));
 
     // Reading the spoke FeeAdapter //
     const spokeFeeAdapter = await spokeInstance.feeAdapter().call();
-    console.log('Spoke Fee Adapter address:', spokeFeeAdapter);
+    console.log('Spoke Fee Adapter address:', tronWeb.address.fromHex(spokeFeeAdapter));
 
     // Reading the gateway instance state //
     // Get the owner of the contract
     const gatewayOwner = await gatewayInstance.owner().call();
-    console.log('Owner of the Spoke Gateway:', gatewayOwner);
+    console.log('Owner of the Spoke Gateway:', tronWeb.address.fromHex(gatewayOwner));
 
     // Get the mailbox address
     const mailbox = await gatewayInstance.mailbox().call();
-    console.log('Mailbox address:', mailbox);
+    console.log('Mailbox address:', tronWeb.address.fromHex(mailbox));
 
     // Getting the ism address
     const ismAddress = await gatewayInstance.interchainSecurityModule().call();
-    console.log('ISM address:', ismAddress);
+    console.log('ISM address:', tronWeb.address.fromHex(ismAddress));
 
     // Get the receiver address
     const receiver = await gatewayInstance.receiver().call();
-    console.log('Receiver address:', receiver);
+    console.log('Receiver address:', tronWeb.address.fromHex(receiver));
 
     // Hub gateway //
     const hubGateway = await gatewayInstance.EVERCLEAR_GATEWAY().call();
-    console.log('Hub Gateway address:', hubGateway);
+    console.log('Hub Gateway address:', tronWeb.address.fromHex(hubGateway));
 
     // Reading XERC20 module state //
     const xerc20ModuleSpoke = await xerc20Instance.spoke().call();
-    console.log('XERC20 module spoke address:', xerc20ModuleSpoke);
+    console.log('XERC20 module spoke address:', tronWeb.address.fromHex(xerc20ModuleSpoke));
 
     // Reading Fee Adapter state //
     const feeAdapterSpoke = await feeAdapterInstance.spoke().call();
-    console.log('Fee Adapter spoke address:', feeAdapterSpoke);
+    console.log('Fee Adapter spoke address:', tronWeb.address.fromHex(feeAdapterSpoke));
 
     const feeAdapterOwner = await feeAdapterInstance.owner().call();
-    console.log('Fee Adapter owner address:', feeAdapterOwner);
+    console.log('Fee Adapter owner address:', tronWeb.address.fromHex(feeAdapterOwner));
 
     const feeAdapterXerc20Module = await feeAdapterInstance.xerc20Module().call();
-    console.log('Fee Adapter XERC20 Module address:', feeAdapterXerc20Module);
+    console.log('Fee Adapter XERC20 Module address:', tronWeb.address.fromHex(feeAdapterXerc20Module));
 
     const feeAdapterFeeSigner = await feeAdapterInstance.feeSigner().call();
-    console.log('Fee Adapter Fee Signer address:', feeAdapterFeeSigner);
+    console.log('Fee Adapter Fee Signer address:', tronWeb.address.fromHex(feeAdapterFeeSigner));
 
     const feeAdapterFeeReceipient = await feeAdapterInstance.feeRecipient().call();
-    console.log('Fee Adapter Fee Recipient address:', feeAdapterFeeReceipient);
+    console.log('Fee Adapter Fee Recipient address:', tronWeb.address.fromHex(feeAdapterFeeReceipient));
 
     console.log('DONE!');
   } catch (err) {
