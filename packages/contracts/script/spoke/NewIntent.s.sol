3a2d05bf5505ad27d2ac075384e52214dee55c8d
preethamr <preethamr@users.noreply.github.com>
2025-10-26 23:52:59 -0700

    chore: clean up lint errors

--- packages/contracts/script/spoke/NewIntent.s.sol
+++ packages/contracts/script/spoke/NewIntent.s.sol
@@ -87,7 +87,9 @@ contract NewIntentBase is Script, ScriptUtils {
     // Try to create intent and catch errors
     try _feeAdapter.newIntent(
       _destinations, _to, _inputAsset, _outputAsset, _amount, Constants.MAX_FEE, _ttl, '', _params
-    ) returns (bytes32 intentId, IEverclear.Intent memory intent) {
+    ) returns (
+      bytes32 intentId, IEverclear.Intent memory intent
+    ) {
       console.log('Intent created successfully!');
       console.logBytes32(intentId);
       // Optionally log intent fields if needed
--- packages/contracts/test/integration/IntegrationBase.t.sol
+++ packages/contracts/test/integration/IntegrationBase.t.sol
@@ -892,10 +892,7 @@ contract IntegrationBase is TestExtended {
     // create new intent
     vm.prank(_user);
 
-    (_intentId, _intent) = spokeChainValues[_origin].feeAdapter
-    .newIntent{
-      value: _ethFee
-    }(
+    (_intentId, _intent) = spokeChainValues[_origin].feeAdapter.newIntent{value: _ethFee}(
       _destinations,
       _user,
       address(_assetOrigin),
