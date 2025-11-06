6c4bea0c5bdc48e0d55587cceddf16180e9ff302
0xHarbs <jamesharbridge@outlook.com>
2025-11-06 13:08:04 +0000

    feat: FeeAdapterV2 deployment scripts

---
+++ packages/contracts/cli/tasks/deployAdaptersV2.ts
@@ -0,0 +1,45 @@
+import { confirm } from '@inquirer/prompts';
+
+import * as c from '../common';
+import { Realm } from '../types';
+
+export async function deployAdaptersV2() {
+  // choose environment (production or staging)
+  const environment = await c.chooseEnvironment();
+
+  // by default, always verify
+  let verify = true;
+  // get domain according to environment and realm
+  const domain = await c.chooseDomain(environment, Realm.SPOKE);
+
+  if (domain == undefined) {
+    return;
+  }
+
+  // if no verification details, choose to continue without verifying
+  if (!domain.verifierAPIKey) {
+    if (
+      await confirm({
+        message: 'Verification details not found. Continue without verifying?',
+        default: false,
+      })
+    ) {
+      verify = false;
+    } else {
+      return;
+    }
+  }
+
+  let script = 'deploy/AdaptersV2.s.sol';
+  script += `:${environment} --rpc-url ${domain.rpc} --chain ${domain.id} --slow`;
+
+  // if verifying, add verification arguments
+  if (verify) {
+    script += ` --etherscan-api-key ${domain.verifierAPIKey!} --verify`;
+  }
+
+  // choose account to broadcast from
+  script += ` --sig "run(string)" ${await c.chooseAccount()}`;
+
+  await c.runFoundryScript(script, Realm.SPOKE);
+}
