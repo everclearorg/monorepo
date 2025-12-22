d8a9f27ef73c51b29e60870841bc648791ef7d18
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2025-12-22 18:47:54 +0530

    fix: creation with distinct message accounts (EVER1-12)

--- packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/intent/new_intent.rs
+++ packages/contracts/solana-spoke/programs/everclear_spoke/src/instructions/intent/new_intent.rs
@@ -53,7 +53,7 @@ pub fn new_intent(
         hyperlane_mailbox: ctx.accounts.hyperlane_mailbox.clone(),
         mailbox_outbox: ctx.accounts.mailbox_outbox.clone(),
         dispatch_authority: ctx.accounts.dispatch_authority.clone(),
-        unique_message_account: ctx.accounts.unique_message_account.clone(),
+        unique_message_account: ctx.accounts.unique_message_account.to_account_info(),
         dispatched_message_pda: ctx.accounts.dispatched_message_pda.clone(),
         igp_program: ctx.accounts.igp_program.clone(),
         igp_program_data: ctx.accounts.igp_program_data.clone(),
@@ -164,6 +164,11 @@ pub fn handle_new_intent<'info>(
     data: Vec<u8>,
     message_gas_limit: u64,
 ) -> Result<IntentAddedEvent> {
+    require!(
+        accounts.unique_message_account.is_signer,
+        SpokeError::InvalidArgument
+    );
+    
     let spoke_state = accounts.spoke_state.clone();
 
     let state = &mut accounts.spoke_state;
@@ -278,7 +283,7 @@ pub fn handle_new_intent<'info>(
         dispatch_authority: accounts.dispatch_authority.to_account_info(),
         // TODO: need to figure out how this is used for the IGP payer and whether this is correct
         sender_wallet: accounts.authority.to_account_info(),
-        unique_message_account: accounts.unique_message_account.to_account_info(),
+        unique_message_account: accounts.unique_message_account.clone(),
         dispatched_message_pda: accounts.dispatched_message_pda.to_account_info(),
         igp_program: accounts.igp_program.clone(),
         igp_program_data: accounts.igp_program_data.to_account_info(),
@@ -329,7 +334,7 @@ pub struct NewIntentAccounts<'info> {
     pub hyperlane_mailbox: Interface<'info, Mailbox>,
     pub mailbox_outbox: AccountInfo<'info>,
     pub dispatch_authority: AccountInfo<'info>,
-    pub unique_message_account: Signer<'info>,
+    pub unique_message_account: AccountInfo<'info>,
     pub dispatched_message_pda: AccountInfo<'info>,
     pub igp_program: Interface<'info, Igp>,
     pub igp_program_data: AccountInfo<'info>,
