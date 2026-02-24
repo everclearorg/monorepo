49c0a81e29a7f3f1109594baae7f6079b9a8bf62
Jintu Das <jintuisbusy@gmail.com>
2026-02-25 00:42:35 +0530

    feat: add syrupusdt and usde asset deployments for mantle

--- packages/contracts/script/MainnetProduction.sol
+++ packages/contracts/script/MainnetProduction.sol
@@ -130,6 +130,12 @@ abstract contract MainnetAssets {
   //////////////////// PTSUSDE
   bytes32 public constant SOLANA_PTSUSDE = 0x05c0ad344d082fe99030a0414acc5726b24c89e1a511eac6e58d809145dd6503;
 
+  ///////////////////// syrupUSDT
+  address public constant MANTLE_SYRUPUSDT = 0x051665f2455116e929b9972c36d23070F5054Ce0;
+
+  ///////////////////// USDe
+  address public constant MANTLE_USDE = 0x5d3a1Ff2b6BAb83b63cd9AD0787074081a52ef34;
+
   ///////////////////// FEE RECIPIENTS
   address public constant FEE_RECIPIENT = 0xac7599880cB5b5eCaF416BEE57C606f15DA5beB8;
 }
