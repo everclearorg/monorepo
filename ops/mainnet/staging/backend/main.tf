8daec8517ac4cb5f9df21b34886c7006dab604fe
Oleg Tsybizov <otsybizov@gmail.com>
2026-03-06 16:28:59 -0600

    fix: use everclear.ninja hosted zone for staging DNS records

--- ops/mainnet/staging/backend/main.tf
+++ ops/mainnet/staging/backend/main.tf
@@ -19,7 +19,8 @@ data "aws_iam_role" "ecr_admin_role" {
 
 
 data "aws_route53_zone" "primary" {
-  zone_id = "Z03634792TWUEHHQ5L0YX"
+  name = "everclear.ninja"
+  private_zone = false
 }
 
 locals {
